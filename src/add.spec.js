0c669b9a32a142d47367a1cd97525b56377fa73a
Shubhneet <kaurshubh099@gmail.com>
2021-04-02 19:29:32 +0530

    added file for head function

--- src/add.spec.js
+++ src/add.spec.js
@@ -1,7 +1,7 @@
 import add from './add';
 import sub from './sub';
 import head from './head';
-describe('Add', () => {
+/*describe('Add', () => {
 
     it('Sum of 0 and 0 is 0', () => {
         expect(add(0, 0)).toEqual(0);
@@ -22,7 +22,7 @@ describe('Sub', () => {
         expect(sub(1, 0)).toEqual(0);
     });
 
-})
+})*/
 describe('head', () => {
 
     it('head of [] is null', () => {
