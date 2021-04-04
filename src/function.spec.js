56733fe1c7be17c7dc8d3dca2884455de7aee289
Shubhneet <kaurshubh099@gmail.com>
2021-04-04 12:56:49 +0530

    modified head and tail functions

--- src/function.spec.js
+++ src/function.spec.js
@@ -11,7 +11,7 @@ describe('head', () => {
 describe('tail', () => {
 
     it('tail of [1,2,3,4] is [2,3,4]', () => {
-        expect(functions.tail([1,2,3,4])).toEqual([1,2,3]);
+        expect(functions.tail([1,2,3,4])).toEqual([2,3,4]);
     });
     it('tail of [] is []', () => {
         expect(functions.tail([])).toEqual([]);
