f3d9f5c0a62611adf960f216c09ba940fa223b0f
Shubhneet <kaurshubh099@gmail.com>
2021-04-04 12:28:31 +0530

    modified head and tail functions

--- src/add.js
+++ src/add.js
@@ -1,8 +1,4 @@
 const add = (first,second) => {
     return first + second;   
 }
-const sub = (first,second) => {
-    return first-second;
-}
 export default add;
-export default sub;
--- src/function.js
+++ src/function.js
@@ -1,3 +1,5 @@
+import { max, min } from "moment";
+
 const functions = {
     head(array1) {
     if (array1.length == 0) {
@@ -10,6 +12,18 @@ const functions = {
         return [];
     }
     return array1.shift();
+    },
+    maximum(array1){
+        return max(array1);
+    },
+    minimum(array1){
+        return min(array1);
+    },
+    identity(value){
+        return value;
+    },
+    cube(value){
+        return value*value*value ; 
     }
 }
-export default functions;
+module.exports  = functions;
